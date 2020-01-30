48a21c7f90ea32c8402cb8b8b6dba052fce4d142
ZhengSheng <j13tw@yahoo.com.tw>
2020-01-30 16:52:05 +0800

    add alert api to telegram

--- app.py
+++ app.py
@@ -44,8 +44,13 @@ dbAirCondictionCurrent = myMongoDb["air_condiction_current"]
 @app.route('/alert/<model>', methods=['POST'])
 def alert(model):
     if (not (model == 'ups' or model == 'icinga' or model == 'librenms')): return {"alert": "api_model_fail"}, status.HTTP_401_UNAUTHORIZED
+    if (model == 'librenms') model = "LibreNMS"
+    if (model == "icinga") model = "IcingaWeb2"
+    if (model == "ups") model = "UPS"
     try:
-        bot.send_message(chat_id=group_id, text=json.loads(str(request.json).replace("'", '"'))["message"])
+        respText = "[" + model + " 監控服務異常告警]\n"
+        respText += json.loads(str(request.json).replace("'", '"'))["message"]
+        bot.send_message(chat_id=group_id, text=respText)
     except:
         return {"alert": "data_fail"}, status.HTTP_401_UNAUTHORIZED
 
