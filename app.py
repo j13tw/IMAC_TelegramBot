12c54b3e08261d2f1d9aaf0fc43e8e73d66351d7
ZhengSheng <j13tw@yahoo.com.tw>
2020-02-01 23:27:00 +0800

    test tag user

--- app.py
+++ app.py
@@ -192,7 +192,7 @@ def getDl303(info):
     if (info == "tc" or info == "all" or info == "temp/humi"):
         tc = dbDl303TC.find_one()
         if (tc['date'] < brokenTime): failList.append('tc')
-        data += "'現在環境溫度: " + str(tc['tc']) + "度'\n"
+        data += "`現在環境溫度: " + str(tc['tc']) + "度`\n"
     if (info == "rh" or info == "all" or info == "temp/humi"):
         rh = dbDl303RH.find_one()
         if (rh['date'] < brokenTime): failList.append('rh')
