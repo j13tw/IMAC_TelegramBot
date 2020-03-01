354747d1ffdf1420a01bbdf80a5e305a22cdc165
ZhengSheng <j13tw@yahoo.com.tw>
2020-03-01 18:00:16 +0800

    test dailyReport-telegram-7

--- app.py
+++ app.py
@@ -61,11 +61,11 @@ def getDailyReport():
             if (dailyReport["error"] in ["power"]): 
                 respText += "`快取失敗`\n"
             else:
-                respText += "`天氣狀態:{0:>10s}`\n".format(dailyReport["Wx"])
-                respText += "`陣風風向:{0:>10s}`\n".format(dailyReport["WD"])
+                respText += "`天氣狀態:\t{0:s}`\n".format(dailyReport["Wx"])
+                respText += "`陣風風向:\t{0:s}`\n".format(dailyReport["WD"])
                 respText += "`平均風速:{0:>5d} 公尺/秒`\n".format(int(dailyReport["WS"]))
                 respText += "`降雨機率:{0:>5d} %`\n".format(int(dailyReport["PoP12h"]))
-                respText += "`舒適指數:{0:>10s}`\n".format(dailyReport["CI"])
+                respText += "`舒適指數:\t{0:s}`\n".format(dailyReport["CI"])
                 respText += "`室外溫度:{0:>5.1f} 度`\n".format(int(dailyReport["T"]))
                 respText += "`體感溫度:{0:>5.1f} 度`\n".format(int(dailyReport["AT"]))
                 respText += "`室外濕度:{0:>5d} %`\n".format(int(dailyReport["RH"]))
