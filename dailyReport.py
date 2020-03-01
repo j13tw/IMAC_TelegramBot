ccb84fa3b5e5778625e04ad1fa35e28ff1ccc8bc
j13tw <j13tw@yhaoo.com.tw>
2020-03-02 01:34:39 +0800

    fix dailyReport-1

--- dailyReport.py
+++ dailyReport.py
@@ -1,26 +1,20 @@
 import datetime
 import requests
 import time
-import configparser
 import json
 
 preDay = 0
 sendReport = 0
 
-# Load data from config.ini file
-config = configparser.ConfigParser()
-config.read('config.ini')
-
-data = {}
-
 while (True):
-    timeJson = json.loads(datetime.datetime.strftime(datetime.datetime.now(), '{"date": "%Y-%m-%d" ,"day":%d, "hour":%H}'))
-    if (int(timeJson["hour"]) == 0 and sendReport == 0):
+    timeJson = json.loads(datetime.datetime.strftime(datetime.datetime.now() + datetime.timedelta(hours=8), '{"day":%d, "hour":%H}'))
+    if (int(timeJson["hour"]) == 8 and sendReport == 0):
         sendReport = 1
         try: 
             requests.get("http://127.0.0.1:5000/dailyReport")
         except: 
             pass
-    if (timeJson["day"] != preDay): sendReport = 0
+    if (timeJson["day"] != preDay): 
+        preDay = timeJson["day"]
+        sendReport = 0
     time.slppe(15)
-    
