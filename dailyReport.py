7fe525b6cae46cd466e0f1a0503f7b55c5066e4d
j13tw <j13tw@yhaoo.com.tw>
2020-03-02 01:44:32 +0800

    fix dailyReport-3

--- dailyReport.py
+++ dailyReport.py
@@ -7,14 +7,15 @@ preDay = 0
 sendReport = 0
 
 while (True):
-    timeJson = json.loads(datetime.datetime.strftime(datetime.datetime.now() + datetime.timedelta(hours=8), '{"day":\"%d\"", "hour":\"%H\""}'))
+    timeJson = json.loads(datetime.datetime.strftime(datetime.datetime.now() + datetime.timedelta(hours=8), '{"day":\"%d\", "hour":\"%H\"}'))
+    print(str(datetime.datetime.now() + datetime.timedelta(hours=8)))
     if (int(timeJson["hour"]) == 8 and sendReport == 0):
         sendReport = 1
         try: 
             requests.get("http://127.0.0.1:5000/dailyReport")
         except: 
             pass
-    if (timeJson["day"] != preDay): 
-        preDay = timeJson["day"]
+    if (int(timeJson["day"]) != preDay): 
+        preDay = int(timeJson["day"])
         sendReport = 0
     time.slppe(15)
