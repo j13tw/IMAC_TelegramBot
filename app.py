17366a643e5ea9295efc5d305ec20e084da760ce
ZhengSheng <j13tw@yahoo.com.tw>
2020-03-06 13:43:29 +0800

    add data-center setting-8

--- app.py
+++ app.py
@@ -60,7 +60,7 @@ settingObject = ""
 device_list = ['溫度', '濕度', 'CO2', '電流', 'DL303', 'ET7044', 'UPS', '冷氣', '環控設備' ,'遠端控制', '每日通報', '服務列表', '服務狀態', '機房輪值', '設定機房\n設備數量', '機房資訊']
 
 # 設定機房資訊定義
-v
+
 setting_json_list = ['cpu', 'ram', 'storage', 'switch', 'sdn', 'pc', 'gpu']
 setting_unit_list = ['Core', 'GB', 'TB', '台', '台', '台', '台', '台']
 
