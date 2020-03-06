b87f416bdb3d05015221c0ca111e999cbecb52bb
ZhengSheng <j13tw@yahoo.com.tw>
2020-03-06 15:44:44 +0800

    add data-center setting-15

--- app.py
+++ app.py
@@ -76,7 +76,7 @@ def getDeviceCount():
     deviceCount = dbDeviceCount.find_one()
     data = "*[機房設備資訊]*\n"
     for x in range(1, len(setting_json_list)):
-        data += "`" + setting_list[x] + ": \t"+ str(deviceCount[setting_json_list[x]]) + "\t" + setting_unit_list[x] + "`\n"
+        data += "`" + setting_list[x] + ": \t"+ str(deviceCount[setting_json_list[x]]) + "\t" + setting_unit_list[x] + "`\n\r"
     return data
     
 
